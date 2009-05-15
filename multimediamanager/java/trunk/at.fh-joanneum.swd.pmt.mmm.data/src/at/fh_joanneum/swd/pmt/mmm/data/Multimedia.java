2eff78344f38dc6124aef1983fb7242938494447
maja.wruss@gmail.com <maja.wruss@gmail.com@a39c6632-308a-11de-9e80-b52b74bdfc52>
2009-05-15 19:39:47 +0000

    neuer File

---
+++ multimediamanager/java/trunk/at.fh-joanneum.swd.pmt.mmm.data/src/at/fh_joanneum/swd/pmt/mmm/data/Multimedia.java
@@ -0,0 +1,17 @@
+package at.fh_joanneum.swd.pmt.mmm.data;
+
+public class Multimedia {
+	
+	String Titel;
+
+	public String getTitel() {
+		return Titel;
+	}
+
+	public void setTitel(String titel) {
+		Titel = titel;
+	}
+	
+	
+
+}
