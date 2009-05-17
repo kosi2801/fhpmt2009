bbe375de55dc22210902430136ce0ac35d0ced18
maja.wruss@gmail.com <maja.wruss@gmail.com@a39c6632-308a-11de-9e80-b52b74bdfc52>
2009-05-17 14:21:27 +0000

    Neue Klasse

---
+++ multimediamanager/java/trunk/at.fh-joanneum.swd.pmt.mmm.data/src/at/fh_joanneum/swd/pmt/mmm/data/MultimediaDataStore.java
@@ -0,0 +1,17 @@
+package at.fh_joanneum.swd.pmt.mmm.data;
+
+public class MultimediaDataStore implements IMultimediaDataStore{
+	
+	private Multimedia multimedia;
+
+	@Override
+	public Multimedia getMultimedia() {
+		return multimedia;
+	}
+
+	@Override
+	public void setMultimedia(Multimedia multimedia) {
+		this.multimedia = multimedia;	
+	}
+
+}
