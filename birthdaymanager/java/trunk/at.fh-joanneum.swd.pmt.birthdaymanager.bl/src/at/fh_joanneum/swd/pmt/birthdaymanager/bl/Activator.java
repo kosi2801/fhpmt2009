fe5f090411ac8cd8b0a237c95d9cce5ec0c23c39
andreas.ueberbacher@gmail.com <andreas.ueberbacher@gmail.com@a39c6632-308a-11de-9e80-b52b74bdfc52>
2009-05-24 13:52:14 +0000

    first backup

--- birthdaymanager/java/trunk/at.fh-joanneum.swd.pmt.birthdaymanager.bl/src/at/fh_joanneum/swd/pmt/birthdaymanager/bl/Activator.java
+++ birthdaymanager/java/trunk/at.fh-joanneum.swd.pmt.birthdaymanager.bl/src/at/fh_joanneum/swd/pmt/birthdaymanager/bl/Activator.java
@@ -2,49 +2,71 @@ package at.fh_joanneum.swd.pmt.birthdaymanager.bl;
 
 import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
+import org.osgi.util.tracker.ServiceTracker;
+
+import at.fh_joanneum.swd.pmt.birthday.data.IUserDataStore;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends Plugin {
+public class Activator extends Plugin
+{
 
+	private ServiceTracker userDataTracker;
 	// The plug-in ID
 	public static final String PLUGIN_ID = "at.fh_joanneum.swd.pmt.birthdaymanager.bl";
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
-	public Activator() {
+	public Activator()
+	{
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
 	 */
-	public void start(BundleContext context) throws Exception {
+	public void start(BundleContext context) throws Exception
+	{
 		super.start(context);
 		plugin = this;
+
+		this.userDataTracker = new ServiceTracker(context,
+				at.fh_joanneum.swd.pmt.birthday.data.IUserDataStore.class.getName(), null);
+		userDataTracker.open();
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
 	 */
-	public void stop(BundleContext context) throws Exception {
+	public void stop(BundleContext context) throws Exception
+	{
 		plugin = null;
 		super.stop(context);
 	}
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
-	public static Activator getDefault() {
+	public static Activator getDefault()
+	{
 		return plugin;
+	}
+
+	public IUserDataStore getStore()
+	{
+		return (IUserDataStore) userDataTracker.getService();
 	}
 
 }
