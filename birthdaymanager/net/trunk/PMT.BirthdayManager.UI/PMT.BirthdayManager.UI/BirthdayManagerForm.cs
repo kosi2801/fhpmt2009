497d8af02d22bbd8f409a907f1186808e40b5194
andreas.ueberbacher@gmail.com <andreas.ueberbacher@gmail.com@a39c6632-308a-11de-9e80-b52b74bdfc52>
2009-06-05 18:17:35 +0000

    1st

---
+++ birthdaymanager/net/trunk/PMT.BirthdayManager.UI/PMT.BirthdayManager.UI/BirthdayManagerForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PMT.BirthdayManager.UI
+{
+    public partial class BirthdayManagerForm : Form
+    {
+        public BirthdayManagerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
