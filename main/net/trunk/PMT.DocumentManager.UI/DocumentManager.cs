dea8e947af539e7a145a3c45875b45284038f311
kosi2801@gmail.com <kosi2801@gmail.com@a39c6632-308a-11de-9e80-b52b74bdfc52>
2009-06-05 18:24:31 +0000

    Adding DocumentManager

---
+++ main/net/trunk/PMT.DocumentManager.UI/DocumentManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PMT.DocumentManager.UI
+{
+    public partial class DocumentManager : Form
+    {
+        public DocumentManager()
+        {
+            InitializeComponent();
+        }
+    }
+}
