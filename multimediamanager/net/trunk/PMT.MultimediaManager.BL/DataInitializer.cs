332e71d0b260be9bbfa9061610beb94f8920ec93
maja.wruss@gmail.com <maja.wruss@gmail.com@a39c6632-308a-11de-9e80-b52b74bdfc52>
2009-06-23 19:52:23 +0000

    neue BL

---
+++ multimediamanager/net/trunk/PMT.MultimediaManager.BL/DataInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PMT.Multimediamanager.Data;
+
+namespace PMT.Multimediamanager.BL
+{
+    public class DataInitializer
+    {
+
+        public void intialize()
+        {
+            DataStore store = DataStore.Instance;
+            List<Multimedia> ms = store.Multimedias;
+
+            Multimedia multimediaI = new Multimedia();
+            multimediaI.Titel = "Ferien am Strand";
+            multimediaI.Typ = MultimediaTyp.IMAGE;
+
+            ms.Add(multimediaI);
+
+            Multimedia multimediaA = new Multimedia();
+            multimediaA.Titel = "Holiday Feeling";
+            multimediaA.Typ = MultimediaTyp.AUDIO;
+
+            ms.Add(multimediaA);
+
+            Multimedia multimediaV = new Multimedia();
+            multimediaV.Titel = "Summer Day";
+            multimediaV.Typ = MultimediaTyp.VIDEO;
+
+            ms.Add(multimediaV);
+
+        }
+    }
+}
