5322cdcf67fb2b1008e9aa65d48420c85b0b88a6
maja.wruss@gmail.com <maja.wruss@gmail.com@a39c6632-308a-11de-9e80-b52b74bdfc52>
2009-06-24 17:45:52 +0000

    Neuer Formular fuer remove

---
+++ multimediamanager/net/trunk/PMT.MultimediaManager.UI/RemoveData.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace PMT.Multimediamanager.UI
+{
+    partial class RemoveData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.mmTitel = new System.Windows.Forms.Label();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.mmTyp = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Möchten Sie wirklich folgende Multimedia \r\naus der Liste entfernen?";
+            // 
+            // mmTitel
+            // 
+            this.mmTitel.AutoSize = true;
+            this.mmTitel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mmTitel.Location = new System.Drawing.Point(26, 64);
+            this.mmTitel.Name = "mmTitel";
+            this.mmTitel.Size = new System.Drawing.Size(41, 13);
+            this.mmTitel.TabIndex = 1;
+            this.mmTitel.Text = "label2";
+            // 
+            // okButton
+            // 
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Location = new System.Drawing.Point(29, 154);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(97, 27);
+            this.okButton.TabIndex = 2;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(161, 154);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(97, 27);
+            this.cancelButton.TabIndex = 3;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Typ:";
+            // 
+            // mmTyp
+            // 
+            this.mmTyp.AutoSize = true;
+            this.mmTyp.Location = new System.Drawing.Point(60, 89);
+            this.mmTyp.Name = "mmTyp";
+            this.mmTyp.Size = new System.Drawing.Size(35, 13);
+            this.mmTyp.TabIndex = 5;
+            this.mmTyp.Text = "label4";
+            // 
+            // RemoveData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 212);
+            this.Controls.Add(this.mmTyp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.mmTitel);
+            this.Controls.Add(this.label1);
+            this.Name = "RemoveData";
+            this.Text = "Entfernen von Multimedia";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label mmTitel;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label mmTyp;
+    }
+}
---
+++ multimediamanager/net/trunk/PMT.MultimediaManager.UI/RemoveData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PMT.Multimediamanager.Data;
+
+namespace PMT.Multimediamanager.UI
+{
+    public partial class RemoveData : Form
+    {
+        public RemoveData()
+        {
+            InitializeComponent();
+        }
+        public RemoveData(Multimedia mm)
+        {
+            InitializeComponent();
+            mmTitel.Text = mm.Titel;
+            mmTyp.Text = mm.Typ.ToString();
+            this.Update();
+
+        }
+
+        
+    }
+}
