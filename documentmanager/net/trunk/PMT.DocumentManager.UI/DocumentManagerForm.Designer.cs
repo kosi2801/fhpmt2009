fc73c489e29dda56b2d5c4575aa1cdeee757a43b
kosi2801@gmail.com <kosi2801@gmail.com@a39c6632-308a-11de-9e80-b52b74bdfc52>
2009-06-25 16:07:40 +0000

    Adding DocumentManager to PMT.Application

---
+++ documentmanager/net/trunk/PMT.DocumentManager.UI/DocumentManagerForm.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace PMT.DocumentManager.UI
+{
+    partial class DocumentManagerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.locationTextBox = new System.Windows.Forms.TextBox();
+            this.ownerTextBox = new System.Windows.Forms.TextBox();
+            this.Location = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.setDefaultsButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(126, 24);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(327, 20);
+            this.nameTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // locationTextBox
+            // 
+            this.locationTextBox.Location = new System.Drawing.Point(126, 61);
+            this.locationTextBox.Name = "locationTextBox";
+            this.locationTextBox.Size = new System.Drawing.Size(327, 20);
+            this.locationTextBox.TabIndex = 2;
+            // 
+            // ownerTextBox
+            // 
+            this.ownerTextBox.Location = new System.Drawing.Point(126, 100);
+            this.ownerTextBox.Name = "ownerTextBox";
+            this.ownerTextBox.Size = new System.Drawing.Size(327, 20);
+            this.ownerTextBox.TabIndex = 3;
+            // 
+            // Location
+            // 
+            this.Location.AutoSize = true;
+            this.Location.Location = new System.Drawing.Point(26, 61);
+            this.Location.Name = "Location";
+            this.Location.Size = new System.Drawing.Size(48, 13);
+            this.Location.TabIndex = 4;
+            this.Location.Text = "Location";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Owner";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(32, 154);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // setDefaultsButton
+            // 
+            this.setDefaultsButton.Location = new System.Drawing.Point(214, 153);
+            this.setDefaultsButton.Name = "setDefaultsButton";
+            this.setDefaultsButton.Size = new System.Drawing.Size(75, 23);
+            this.setDefaultsButton.TabIndex = 7;
+            this.setDefaultsButton.Text = "Set Defaults";
+            this.setDefaultsButton.UseVisualStyleBackColor = true;
+            this.setDefaultsButton.Click += new System.EventHandler(this.setDefaultsButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(377, 153);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 8;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // DocumentManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 192);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.setDefaultsButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Location);
+            this.Controls.Add(this.ownerTextBox);
+            this.Controls.Add(this.locationTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameTextBox);
+            this.Name = "DocumentManagerForm";
+            this.Text = "DocumentManagerForm";
+            this.Load += new System.EventHandler(this.DocumentManagerForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox locationTextBox;
+        private System.Windows.Forms.TextBox ownerTextBox;
+        private System.Windows.Forms.Label Location;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button setDefaultsButton;
+        private System.Windows.Forms.Button closeButton;
+    }
+}
---
+++ documentmanager/net/trunk/PMT.DocumentManager.UI/DocumentManagerForm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PMT.DocumentManager.Data;
+using PMT.DocumentManager.BL;
+
+namespace PMT.DocumentManager.UI
+{
+    public partial class DocumentManagerForm : Form
+    {
+        public DocumentManagerForm()
+        {
+            InitializeComponent();
+            updateData();
+        }
+
+        private void updateData()
+        {
+            DataStore store = DataStore.Instance;
+            if (store.DocumentData != null)
+            {
+                nameTextBox.Text = store.DocumentData.Name;
+                locationTextBox.Text = store.DocumentData.Location;
+                ownerTextBox.Text = store.DocumentData.Owner;
+            }
+            else
+            {
+                nameTextBox.Text = "";
+                locationTextBox.Text = "";
+                ownerTextBox.Text = "";
+            }
+
+        }
+
+        private void DocumentManagerForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            DataStore store = DataStore.Instance;
+            store.DocumentData.Name = nameTextBox.Text;
+            store.DocumentData.Location = locationTextBox.Text;
+            store.DocumentData.Owner = ownerTextBox.Text;
+            updateData();
+        }
+
+        private void setDefaultsButton_Click(object sender, EventArgs e)
+        {
+            new DataInitializer().intialize();
+            updateData();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            this.Dispose();
+        }
+    }
+}
