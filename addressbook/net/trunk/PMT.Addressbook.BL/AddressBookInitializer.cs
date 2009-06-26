95a418e3ca3e70de3c123822025dfac8b13047c2
Zeddicus_007@hotmail.com <Zeddicus_007@hotmail.com@a39c6632-308a-11de-9e80-b52b74bdfc52>
2009-06-26 07:04:01 +0000

    Initial import

---
+++ addressbook/net/trunk/PMT.Addressbook.BL/AddressBookInitializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PMT.AddressBook.Data;
+
+namespace PMT.AddressBook.BL
+{
+    public class AddressBookInitializer
+    {
+        public void initialize()
+        {
+            AddressDataStore store = AddressDataStore.Instance;
+
+            Address addr = new Address();
+            addr.Name = "Max Mustermann";
+            addr.Street = "Musterstraße 12";
+            addr.Zip = "A-12345";
+            addr.City = "Musterstadt";
+            addr.Country = "Musterland";
+            addr.Email = "max.mustermann@mustercompany.mu";
+            addr.Phone = "123-456 789";
+            addr.Mobile = "9784/512 30 24";
+
+            store.Addresses.Clear();
+            store.addAddress(addr);
+        }
+    }
+}
